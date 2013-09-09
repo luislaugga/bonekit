c0825bf8591df8122f3a97d4089e97f67e1e3c86
Luis Laugga <luis@laugga.com>
2013-09-09 15:21:17 +0100

    Add lib/bonekit to create_makefile in extconf.rb;

--- ext/bonekit/extconf.rb
+++ ext/bonekit/extconf.rb
@@ -1,6 +1,8 @@
 # Loads mkmf which is used to make makefiles for Ruby extensions
 require 'mkmf'
 
+$CFLAGS << %[ -I.. -Wall -O3 -g -std=c99]
+
 # Give it a name
 extension_name = 'bonekit'
 
@@ -8,4 +10,4 @@ extension_name = 'bonekit'
 dir_config(extension_name)
 
 # Do the work
-create_makefile(extension_name)
+create_makefile(extension_name/extension_name)
