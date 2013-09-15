38b53c5649e39bf88ef63bd86ea42e90ea98e529
Luis Laugga <luis@laugga.com>
2013-09-15 11:29:50 +0100

    Update examples;

--- examples/basics/switch.rb
+++ examples/basics/switch.rb
@@ -4,11 +4,7 @@ switchPin = Pin.new 30, Input
 ledPin = Pin.new 31, Output
 
 loop do
- if switchPin.value == 0 # switch is pressed
-  ledPin.value = 1
- else
-  ledPin.value = 0
- end
- sleep 0.01
+  ledPin.value = switchPin.value 
+  sleep 0.01
 end
 
