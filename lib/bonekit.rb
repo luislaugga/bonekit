fc40aba26f5f8f7e757dd3f9a7aa92367135ff07
Luis Laugga <luis@laugga.com>
2013-09-27 01:00:47 +0100

    Update version to 0.0.3; Update require location to bonekit.so shared object file;

--- lib/bonekit.rb
+++ lib/bonekit.rb
@@ -1,9 +1,3 @@
 base_path = File.expand_path(File.dirname(__FILE__))
 require File.join(base_path, 'bonekit', 'version')
-begin
-  m = /(\d+.\d+)/.match(RUBY_VERSION)
-  ver = m[1]
-  require File.join(base_path, 'bonekit', ver, 'bonekit')
-rescue LoadError
-  require File.join(base_path, 'bonekit', 'bonekit')
-end
+require File.join(base_path, 'bonekit')
